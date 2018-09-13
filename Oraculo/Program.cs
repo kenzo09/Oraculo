df6e7cd7aa235b7ec0d895c50871c1a5699afee2
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-12 21:58:28 -0300

    Add project files.

---
+++ Oraculo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oraculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
